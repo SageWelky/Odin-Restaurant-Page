d2ba855b863c1496e82202ab47b0a3ab8ea43d2b
Sage Welky <ehrenwelky@gmail.com>
2024-10-11 10:21:42 -0500

    updated the tabLogic import, as it was not using a function but the general page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import kebabPic from './assets/kebabPic.svg';
 
-import tabSwap from './tabLogic';
+import './tabLogic';
 
 let logoContainer = document.querySelector('#logo-container');
 const myIcon = new Image();
